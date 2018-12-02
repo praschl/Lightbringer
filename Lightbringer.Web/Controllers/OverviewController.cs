d8e7ea96f8e0309964fb1d948ed4b2d71deaa616
Michael Praschl <michael.praschl@gmail.com>
2018-12-02 16:44:13 +0100

    ui and controller for adding service host

---
+++ Lightbringer.Web/Controllers/OverviewController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Lightbringer.Wcf.Contracts.Daemons;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lightbringer.Web.Controllers
+{
+    public class OverviewController : Controller
+    {
+        private readonly IDaemonService _daemonService;
+
+        [BindProperty]
+        public OverviewViewModel OverviewViewModel { get; set; } = new OverviewViewModel();
+
+        [TempData] public string Message { get; set; }
+
+        public OverviewController(IDaemonService daemonService)
+        {
+            _daemonService = daemonService;
+        }
+
+        public IActionResult Index()
+        {
+            var response = _daemonService.GetAllDaemons(new AllDaemonsRequest());
+
+            OverviewViewModel.AllDaemons = response.Daemons;
+            OverviewViewModel.Message = Message;
+
+            return View(OverviewViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddServiceHost()
+        {
+            Message = "Servicehost added.";
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+
+    public class OverviewViewModel
+    {
+        public IReadOnlyCollection<DaemonDto> AllDaemons { get; set; }
+
+        public string AddServiceHost { get; set; }
+  
+        public string Message { get; set; }
+    }
+}
